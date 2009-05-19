7500d9544e31a60e3d08b5ea54d60744405e4680
Wade Simmons <wsimmons@gmail.com>
2009-05-19 00:02:11 -0600

    more js fun

--- js/splash.js
+++ js/splash.js
@@ -1,6 +1,7 @@
 (function(){
   var bits;
   
+  /*
   window.code_splash = {
     start: function() {
       var div = $('#code');
@@ -19,6 +20,35 @@
           this.style.left += 1;
         })
       }, 500);
+    }
+  }
+  */
+  
+  var div2;
+  window.code_splash = {
+    start: function() {
+      var div = $('#code');
+      var pos = div.position();
+      //div.prepend("<div class='bits' style='height:300px;width:100px;float:left'></div>");
+            div.prepend("<div class='bits' style='color:#333;height:300px;width:200px;position:absolute;left:"+(pos.left+5)+"px;top:"+(pos.top+5)+"px'></div>");
+      div2 = div.find('.bits');
+      
+      //prep
+      for (var i = 0; i < 100; ++i) {
+        window.code_splash.animate();
+      }
+      
+      window.setInterval(function() {
+        window.code_splash.animate();
+      }, 100);
+    },
+    
+    animate: function() {
+      var text = div2.text();
+      if (text.length > 200) {
+        text = text.substring(2);
+      }
+      div2.text(text+' '+(Math.random() < 0.5 ? '0' : '1'))
     }
   }
   

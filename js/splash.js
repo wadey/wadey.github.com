c31ab7a68fd45ca2cfaf3d756d010d0980893c2d
Wade Simmons <wsimmons@gmail.com>
2009-05-18 21:12:36 -0600

    work in progress for new site

---
+++ js/splash.js
@@ -0,0 +1,13 @@
+(function(){
+  window.photos_splash = {
+    start: function() {
+      var div = $('#code');
+      //div.append("test");
+      //alert("test");
+    }
+  }
+  
+  //$(document).ready(function() {
+  //  window.photos_splash.start();
+  //})
+})();
